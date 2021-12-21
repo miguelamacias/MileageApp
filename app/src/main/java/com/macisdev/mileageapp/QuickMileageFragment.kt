d544dedee9612ac1eccdec6d9ad99db71dceb6ea
Miguel Angel Macias <miguel.a.macias.13@gmail.com>
2021-12-21 17:14:16 +0100

    Initial commit
    -Quick mileage functionality implemented

---
+++ app/src/main/java/com/macisdev/mileageapp/QuickMileageFragment.kt
@@ -0,0 +1,44 @@
+package com.macisdev.mileageapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.macisdev.mileageapp.databinding.FragmentQuickMileageBinding
+import java.util.*
+
+class QuickMileageFragment : Fragment() {
+	private lateinit var gui: FragmentQuickMileageBinding
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		gui = FragmentQuickMileageBinding.inflate(inflater, container, false)
+
+		return gui.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		gui.calculateMileageButton.setOnClickListener {
+			try {
+				val kilometres = gui.kilometresEditText.text.toString().toDouble()
+				val litres = gui.litresEditText.text.toString().toDouble()
+				val mileage = 100 * litres / kilometres
+
+				if (mileage > 0) {
+					gui.mileageResultEditText.setText(String.format(Locale.getDefault(),"%.2f", mileage))
+				} else {
+					gui.mileageResultEditText.setText(R.string.wrong_value)
+				}
+
+			} catch (e: NumberFormatException) {
+				gui.mileageResultEditText.setText(R.string.wrong_value)
+			}
+		}
+	}
+
+	companion object {
+		fun newInstance() = QuickMileageFragment()
+	}
+}
---
+++ app/src/main/java/com/macisdev/mileageapp/QuickMileageViewModel.kt
@@ -0,0 +1,7 @@
+package com.macisdev.mileageapp
+
+import androidx.lifecycle.ViewModel
+
+class QuickMileageViewModel : ViewModel() {
+	// TODO: Implement the ViewModel
+}
