d57bc2888cb6ad1dd55efb4f727624cb22af4d85
Miguel Angel Macias <miguel.a.macias.13@gmail.com>
2022-01-19 16:56:52 +0100

    Version 1.0 Beta 07 - Even more GUI improvements
    -New App Icon
    -Margin added to AddVehicleFragment dialog
    -Quick mileage fragment converted to dialog

--- app/src/main/java/com/macisdev/mileageapp/QuickMileageFragment.kt
+++ app/src/main/java/com/macisdev/mileageapp/QuickMileageFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
 import com.macisdev.mileageapp.databinding.FragmentQuickMileageBinding
 import java.text.DecimalFormat
 import java.util.*
 
-class QuickMileageFragment : Fragment() {
+class QuickMileageFragment : DialogFragment() {
 	private lateinit var gui: FragmentQuickMileageBinding
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
